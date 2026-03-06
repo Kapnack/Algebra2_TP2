cdd434530cbcc9d6293aabde75186ed969691f3c
Kapnack <180976325+Kapnack@users.noreply.github.com>
2026-03-05 21:35:21 -0300

    Update Plane3.cs

--- Assets/Scripts/Plane3.cs
+++ Assets/Scripts/Plane3.cs
@@ -6,28 +6,22 @@ public struct Plane3
 {
     public Vec3 normal;
     public float d;
-    
+
     public static Plane3 BisectorPlane(Vec3 a, Vec3 b)
     {
         Vec3 dir = b - a;
-        Vec3 mid = new Vec3((a.x + b.x) * 0.5f, (a.y + b.y) * 0.5f, (a.z + b.z) * 0.5f);
+        Vec3 mid = new Vec3((a + b) * 0.5f);
         return new Plane3(dir, mid);
     }
-    
+
     public Plane3(Vec3 normal, Vec3 pointOnPlane)
     {
-        float mag = normal.magnitude;
-        
-        if (mag > Vec3.epsilon)
-            this.normal = normal / mag;
-        else
-            this.normal = new Vec3(0, 1, 0);
-        
+        this.normal = normal.normalized;
         d = Vec3.Dot(this.normal, pointOnPlane);
     }
-    
+
     public bool IsPositiveSide(Vec3 p) => SignedDistance(p) > 0f;
-    
+
     public float SignedDistance(Vec3 p) => Vec3.Dot(normal, p) - d;
-    
+
 }
